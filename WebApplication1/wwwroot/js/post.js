69e6f7caac02e71f2d19b5ff8ab627cd26213082
autyauth <thanakornrattanapornchai@gmail.com>
2023-04-16 02:20:25 +0700

    แก้หน้าMainPose ให้มีการสร้าง pose แล้วก็คอมเม้น แล้วก็มีสีต่างๆแล้ว สวยแล้ว

---
+++ WebApplication1/wwwroot/js/post.js
@@ -0,0 +1,11 @@
+﻿function showComment(id) {
+    console.log("click");
+    var main = document.getElementById(id);
+    console.log(main.style.display);
+    if (main.style.display === "block") {
+        main.style.display = "none";
+    } else {
+        main.style.display = "block";
+    }
+
+}
