115d49cbd4ad62372d0992a9f81144e3f60cc82f
Northy <k.thaithat007@gmail.com>
2023-04-16 13:23:15 +0700

    back to correct

--- WebApplication1/wwwroot/js/post.js
+++ WebApplication1/wwwroot/js/post.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 function showComment(id) {
-=======
-﻿function showComment(id) {
->>>>>>> a9a73f4fe22af47f5396286adc56b736ec198b22
     console.log("click");
     var main = document.getElementById(id);
     console.log(main.style.display);
@@ -11,5 +7,4 @@ function showComment(id) {
     } else {
         main.style.display = "block";
     }
-
 }
