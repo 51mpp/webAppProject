52b098f02b65778a7b798f19aa043390a640d482
Northy <k.thaithat007@gmail.com>
2023-04-22 15:53:49 +0700

    edit id js

--- WebApplication1/wwwroot/js/post.js
+++ WebApplication1/wwwroot/js/post.js
@@ -1,31 +1,34 @@
-function showComment(id, current, max) {
+function showComment(id, current, max, state) {
     console.log("click");
     var main = document.getElementById(id);
+    console.log(main);
     console.log(main.style.display);
-    if (main.style.display === "block") {
-        main.style.display = "none";
-    } else {
-        main.style.display = "block";
+    if (state === 1) {
+        if (main.style.display === "block") {
+            main.style.display = "none";
+        } else {
+            main.style.display = "block";
+        }
+        if (current == max) {
+            main.style.textAlign = "center";
+            main.innerHTML = "เต็มแล้วจ้า";
+        }
     }
-    if (current == max) {
-        main.style.textAlign = "center";
-        main.innerHTML = "เต็มแล้วจ้า";
-    }
-
 }
 
-function showQR(id) {
+function showQR(id, state) {
     console.log("scan");
     var image = document.getElementById(id);
-    if (image.style.display === "none") {
-        image.style.display = "block";
-        image.style.marginLeft = "auto";
-        image.style.marginRight = "auto";
+    if (state === 2) {
+        if (image.style.display === "none") {
+            image.style.display = "block";
+            image.style.marginLeft = "auto";
+            image.style.marginRight = "auto";
 
-    } else {
-        image.style.display = "none";
+        } else {
+            image.style.display = "none";
+        }
     }
-
 }
 
 
